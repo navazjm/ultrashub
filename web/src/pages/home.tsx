3dd3c255e838769825a62cddc4e4e5d267383046
navazjm <michaelnavs@gmail.com>
2024-01-27 19:15:40 -0600

    web - fix npm run build issues

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,11 +1,9 @@
 import { MatchesComponent } from "@/components/features/matches/matches.component";
-import { useParams } from "react-router-dom";
 
 export const HomePage = () => {
-    const { date } = useParams();
     return (
         <>
-            <MatchesComponent date={date} />
+            <MatchesComponent />
         </>
     );
 };
