f64fe0197066259225499c9bf7943fb19f13b61c
navazjm <michaelnavs@gmail.com>
2024-01-28 19:03:01 -0600

    web - sticky navbar

--- web/src/layouts/navbar/navbar.component.tsx
+++ web/src/layouts/navbar/navbar.component.tsx
@@ -6,20 +6,18 @@ import "./navbar.component.css";
 
 export const NavbarComponent = () => {
     return (
-        <>
-            <div className="w-full max-w-full min-w-full shadow-xl">
-                <NavigationMenu className="w-10/12 max-w-10/12 min-w-10/12 m-auto justify-between py-1">
-                    <NavLink to="/" className="flex gap-x-2 content-center">
-                        <img src={ultrashubLogo} width="30px" />
-                        <h3 className="text-2xl font-black">UltrasHub</h3>
-                    </NavLink>
-                    <NavigationMenuList>
-                        <NavigationMenuItem>
-                            <ThemeToggleComponent />
-                        </NavigationMenuItem>
-                    </NavigationMenuList>
-                </NavigationMenu>
-            </div>
-        </>
+        <div className="w-full max-w-full min-w-full shadow-xl sticky top-0 bg-background z-50">
+            <NavigationMenu className="w-10/12 max-w-10/12 min-w-10/12 m-auto justify-between py-1">
+                <NavLink to="/" className="flex gap-x-2 content-center">
+                    <img src={ultrashubLogo} width="30px" />
+                    <h3 className="text-2xl font-black">UltrasHub</h3>
+                </NavLink>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <ThemeToggleComponent />
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </div>
     );
 };
