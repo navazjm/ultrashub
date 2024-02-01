fbccadb439711b32e2c3573bafc958b3e08fd585
navazjm <michaelnavs@gmail.com>
2024-02-01 15:46:57 -0600

    web - implement match quick info component

--- web/src/components/features/match/match-quick-info/match-quick-info.component.tsx
+++ web/src/components/features/match/match-quick-info/match-quick-info.component.tsx
@@ -1,7 +1,9 @@
 import { Match } from "@/components/common/api-football-response";
 import { BackNavigationComponent } from "@/components/common/back-navigation/back-navigation.component";
 import { IProps } from "@/components/common/types";
-import { Card } from "@/components/ui/card";
+import { Card, CardHeader, CardFooter, CardContent } from "@/components/ui/card";
+import { MatchQuickInfoCardContentComponent } from "./match-quick-info-card-content/match-quick-info-card-content.component";
+import { MatchQuickInfoFixtureDetailsComponent } from "./match-quick-info-fixture-details/match-quick-info-fixture-details.component";
 
 export interface IMatchQuickInfoComponentProps extends IProps {
     match: Match;
@@ -15,7 +17,25 @@ export const MatchQuickInfoComponent = (props: IMatchQuickInfoComponentProps) =>
         <>
             <BackNavigationComponent title={backNavTitle} />
             <Card className="w-full mt-3">
-                <div>Hello from match quick info component</div>
+                {/** Show fixture details in header for larger devices */}
+                <CardHeader className="hidden sm:flex flex-row items-center flex-wrap opacity-60 gap-5 space-y-0 text-xs">
+                    <MatchQuickInfoFixtureDetailsComponent
+                        fixture={props.match.fixture}
+                        hasStarted={!!props.hasStarted}
+                    />
+                </CardHeader>
+
+                <CardContent className="p-6 pb-0 sm:pt-0 sm:pb-6">
+                    <MatchQuickInfoCardContentComponent match={props.match} hasStarted={props.hasStarted} />
+                </CardContent>
+
+                {/** Show fixture details in footer for smaller devices */}
+                <CardFooter className="sm:hidden flex flex-row items-center flex-wrap opacity-60 gap-3 space-y-0 text-xs">
+                    <MatchQuickInfoFixtureDetailsComponent
+                        fixture={props.match.fixture}
+                        hasStarted={!!props.hasStarted}
+                    />
+                </CardFooter>
             </Card>
         </>
     );
