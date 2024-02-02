bcbb4cec08919730593ff1ef02920a20889403f0
navazjm <michaelnavs@gmail.com>
2024-02-01 21:48:10 -0600

    web - main and navbar element width for xl breakpoint

--- web/src/layouts/layout.component.tsx
+++ web/src/layouts/layout.component.tsx
@@ -7,7 +7,7 @@ export const Layout = ({ children }: IProps) => {
     return (
         <>
             <NavbarComponent />
-            <main className="md:w-10/12 mx-auto my-3 px-2 md:px-0">{children}</main>
+            <main className="md:w-10/12 xl:w-7/12 mx-auto my-3 px-2 md:px-0">{children}</main>
             <Toaster />
         </>
     );
