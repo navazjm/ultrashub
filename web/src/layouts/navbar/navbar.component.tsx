1e6f9eb80900297acb579a9b67b55b637e75f526
navazjm <michaelnavs@gmail.com>
2024-02-02 00:25:56 -0600

    web - updated navbar shadow styles

--- web/src/layouts/navbar/navbar.component.tsx
+++ web/src/layouts/navbar/navbar.component.tsx
@@ -6,7 +6,7 @@ import "./navbar.component.css";
 
 export const NavbarComponent = () => {
     return (
-        <div className="w-full max-w-full min-w-full shadow-xl sticky top-0 bg-background z-10">
+        <div className="w-full max-w-full min-w-full shadow-sm shadow-muted sticky top-0 bg-background z-10">
             <NavigationMenu className="w-10/12 max-w-10/12 min-w-10/12 xl:w-7/12 xl:min-w-7/12 xl:max-w-7/12 m-auto justify-between py-1">
                 <NavLink to="/" className="flex gap-x-2 content-center">
                     <img src={ultrashubLogo} width="30px" />
