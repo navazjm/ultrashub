6f53173865267113f910c8cf035fa51282f2ef58
navazjm <michaelnavs@gmail.com>
2024-02-05 15:06:02 -0600

    web - cache h2h in memory to prevent multiple api calls

--- web/src/components/features/match/match-h2h/match-h2h.hooks.tsx
+++ web/src/components/features/match/match-h2h/match-h2h.hooks.tsx
@@ -3,12 +3,14 @@ import { AxiosResponse } from "axios";
 import { Match, MatchResponse } from "@/components/common/api-football-response";
 import axios from "@/lib/axios";
 
+let matchesCache: Match[] = [];
+
 export const useMatchH2H = (homeTeamID: number, awayTeamID: number) => {
-    const [matches, setMatches] = useState<Match[]>([]);
-    const [status, setStatus] = useState<"loading" | "success" | "error">("loading");
+    const [matches, setMatches] = useState<Match[]>(matchesCache);
+    const initialStatus = matchesCache.length > 1 ? "success" : "loading";
+    const [status, setStatus] = useState<"loading" | "success" | "error">(initialStatus);
 
     useEffect(() => {
-        setMatches([]);
         const fetchH2H = async () => {
             try {
                 const resp = await axios.get<any, AxiosResponse<MatchResponse>>("/apifootball/fixtures/headtohead", {
@@ -22,11 +24,14 @@ export const useMatchH2H = (homeTeamID: number, awayTeamID: number) => {
                 const matches = resp.data.response;
                 setMatches(matches);
                 setStatus("success");
+                matchesCache = matches;
             } catch (err) {
                 setStatus("error");
             }
         };
-        fetchH2H();
+        if (matchesCache.length < 1) {
+            fetchH2H();
+        }
     }, []);
 
     const isLoading = status === "loading";
