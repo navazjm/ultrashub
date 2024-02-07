b5993c1ecd0b3b7743e9929d1b44ca4debe4a509
navazjm <michaelnavs@gmail.com>
2024-02-07 09:49:05 -0600

    web - h2h stats component

--- web/src/components/features/match/match-h2h/match-h2h.tsx
+++ web/src/components/features/match/match-h2h/match-h2h.tsx
@@ -2,6 +2,7 @@ import { Spinner } from "@/components/ui/spinner";
 import { useMatchH2H } from "./match-h2h.hooks";
 import { IMatchTeam } from "@/components/common/api-football-response";
 import { MatchH2HRecentMatchesComponent } from "./match-h2h-recent-matches/match-h2h-recent-matches";
+import { MatchH2HStatsComponent } from "./match-h2h-stats/match-h2h-stats";
 
 interface IMatchH2HComponentProps {
     homeTeam: IMatchTeam;
@@ -24,6 +25,7 @@ export const MatchH2HComponent = (props: IMatchH2HComponentProps) => {
     return (
         <>
             <h3 className="text-center text-3xl font-bold my-5">Head-to-Head</h3>
+            <MatchH2HStatsComponent matches={matches} homeTeam={props.homeTeam} awayTeam={props.awayTeam} />
             <h3 className="text-center text-3xl font-bold my-5">Recent Matches</h3>
             <MatchH2HRecentMatchesComponent matches={matches} />
         </>
