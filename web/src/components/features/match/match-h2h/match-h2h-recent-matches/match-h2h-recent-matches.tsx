beb2e77ce3388b3334067e9bf63096b589249a29
navazjm <michaelnavs@gmail.com>
2024-02-06 20:28:27 -0600

    web - extracted match h2h recent matches into seperate component

---
+++ web/src/components/features/match/match-h2h/match-h2h-recent-matches/match-h2h-recent-matches.tsx
@@ -0,0 +1,52 @@
+import { NavLink } from "react-router-dom";
+import { IMatch } from "@/components/common/api-football-response";
+import { MatchToolbox } from "@/components/common/toolbox/match";
+import { DateToolbox } from "@/components/common/toolbox/date";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { MatchScorelineComponent } from "../../match-scoreline/match-scoreline";
+
+interface IMatchH2HRecentMatchesComponentProps {
+    matches: IMatch[];
+}
+
+export const MatchH2HRecentMatchesComponent = (props: IMatchH2HRecentMatchesComponentProps) => {
+    const last5Matches = props.matches
+        .filter((match) => {
+            return MatchToolbox.hasMatchStarted(match.fixture.status.short);
+        })
+        .slice(-5)
+        .reverse();
+
+    return (
+        <section className="flex flex-col gap-2">
+            {last5Matches.map((match) => (
+                <NavLink to={`/match/${match.fixture.id}`} key={match.fixture.id}>
+                    <Card className="hover:bg-muted">
+                        <CardHeader className="flex-1 flex flex-row justify-between items-center font-thin text-sm p-2">
+                            <section>
+                                {`${DateToolbox.getDayOfTheWeek(new Date(match.fixture.date).getDay())} 
+                                      ${new Date(match.fixture.date).toLocaleDateString()}`}
+                            </section>
+                            <section>{`${match.league.season} ${match.league.name}`}</section>
+                        </CardHeader>
+                        <CardContent className="flex justify-center items-start p-2 pt-0">
+                            <section
+                                className={`flex-1 flex justify-end items-center gap-2 mr-2 ${match.teams.away.winner && "opacity-40"}`}
+                            >
+                                <img src={match.teams.home.logo} className="w-[25px] object-scale-down" />
+                                <h3 className="text-lg font-bold hidden sm:block">{match.teams.home.name}</h3>
+                            </section>
+                            <MatchScorelineComponent match={match} fontSize="text-lg" />
+                            <section
+                                className={`flex-1 flex justify-start items-center gap-2 ml-2 ${match.teams.home.winner && "opacity-40"}`}
+                            >
+                                <h3 className="text-lg font-bold hidden sm:block">{match.teams.away.name}</h3>
+                                <img src={match.teams.away.logo} className="w-[25px] object-scale-down" />
+                            </section>
+                        </CardContent>
+                    </Card>
+                </NavLink>
+            ))}
+        </section>
+    );
+};
--- web/src/components/features/match/match-h2h/match-h2h.tsx
+++ web/src/components/features/match/match-h2h/match-h2h.tsx
@@ -1,11 +1,7 @@
 import { Spinner } from "@/components/ui/spinner";
 import { useMatchH2H } from "./match-h2h.hooks";
 import { IMatchTeam } from "@/components/common/api-football-response";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { MatchScorelineComponent } from "../match-scoreline/match-scoreline";
-import { MatchToolbox } from "@/components/common/toolbox/match";
-import { NavLink } from "react-router-dom";
-import { DateToolbox } from "@/components/common/toolbox/date";
+import { MatchH2HRecentMatchesComponent } from "./match-h2h-recent-matches/match-h2h-recent-matches";
 
 interface IMatchH2HComponentProps {
     homeTeam: IMatchTeam;
@@ -25,47 +21,11 @@ export const MatchH2HComponent = (props: IMatchH2HComponentProps) => {
         return <p className="text-center my-2">No head-to-head stats found. Try again later.</p>;
     }
 
-    const last5Matches = matches
-        .filter((match) => {
-            return MatchToolbox.hasMatchStarted(match.fixture.status.short);
-        })
-        .slice(-5)
-        .reverse();
-
     return (
         <>
             <h3 className="text-center text-3xl font-bold my-5">Head-to-Head</h3>
             <h3 className="text-center text-3xl font-bold my-5">Recent Matches</h3>
-            <section className="flex flex-col gap-2">
-                {last5Matches.map((match) => (
-                    <NavLink to={`/match/${match.fixture.id}`} key={match.fixture.id}>
-                        <Card className="hover:bg-muted">
-                            <CardHeader className="flex-1 flex flex-row justify-between items-center font-thin text-sm p-2">
-                                <section>
-                                    {`${DateToolbox.getDayOfTheWeek(new Date(match.fixture.date).getDay())} 
-                                      ${new Date(match.fixture.date).toLocaleDateString()}`}
-                                </section>
-                                <section>{`${match.league.season} ${match.league.name}`}</section>
-                            </CardHeader>
-                            <CardContent className="flex justify-center items-start p-2 pt-0">
-                                <section
-                                    className={`flex-1 flex justify-end items-center gap-2 mr-2 ${match.teams.away.winner && "opacity-40"}`}
-                                >
-                                    <img src={match.teams.home.logo} className="w-[25px] object-scale-down" />
-                                    <h3 className="text-lg font-bold hidden sm:block">{match.teams.home.name}</h3>
-                                </section>
-                                <MatchScorelineComponent match={match} fontSize="text-lg" />
-                                <section
-                                    className={`flex-1 flex justify-start items-center gap-2 ml-2 ${match.teams.home.winner && "opacity-40"}`}
-                                >
-                                    <h3 className="text-lg font-bold hidden sm:block">{match.teams.away.name}</h3>
-                                    <img src={match.teams.away.logo} className="w-[25px] object-scale-down" />
-                                </section>
-                            </CardContent>
-                        </Card>
-                    </NavLink>
-                ))}
-            </section>
+            <MatchH2HRecentMatchesComponent matches={matches} />
         </>
     );
 };
