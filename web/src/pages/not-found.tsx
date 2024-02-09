4d707d7b20bfbd5e236331c75f97348fdc8b478b
navazjm <michaelnavs@gmail.com>
2024-02-09 16:41:26 -0600

    web - updated not found page

--- web/src/pages/not-found.tsx
+++ web/src/pages/not-found.tsx
@@ -1,3 +1,16 @@
+import { NavLink } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+
 export const NotFoundPage = () => {
-    return <h3>404 Page not found</h3>;
+    return (
+        <section className="w-90 h-90 flex flex-col justify-center items-center gap-3 p-5">
+            <section className="flex flex-col justify-center items-center">
+                <h3 className="text-4xl font-black">404 Not Found</h3>
+                <p className="font-bold">Looks like you ended up at the wrong pitch</p>
+            </section>
+            <Button variant="outline" asChild>
+                <NavLink to="/">Go Home</NavLink>
+            </Button>
+        </section>
+    );
 };
