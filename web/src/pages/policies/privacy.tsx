b881acb8c4b2ad4cb55d3927140a9a562e4d0d5d
navazjm <michaelnavs@gmail.com>
2024-02-09 22:16:20 -0600

    web - updated pages/policies/privacy

--- web/src/pages/policies/privacy.tsx
+++ web/src/pages/policies/privacy.tsx
@@ -1,7 +1,344 @@
 export const PrivacyPolicyPage = () => {
     return (
         <>
-            <h1>Privacy</h1>
+            <section className="space-y-5 my-5">
+                <p>Effective date: February 9, 2024</p>
+                <p>Last updated: February 9, 2024</p>
+                <p>
+                    Thanks for trusting UltrasHub ('UltrasHub', 'we', 'us', 'our') with your personal information! We
+                    take our responsibility to you very seriously, and so this Privacy Statement describes how we handle
+                    your data.
+                </p>
+                <p>
+                    This Privacy Statement applies to all websites we own and operate and to all services we provide
+                    (collectively, the 'Services'). So...PLEASE READ THIS PRIVACY STATEMENT CAREFULLY. By using the
+                    Services, you are expressly and voluntarily accepting the terms and conditions of this Privacy
+                    Statement and our Terms of Service, which include allowing us to process information about you.
+                </p>
+                <p>
+                    Under this Privacy Statement, we are the data controller responsible for processing your personal
+                    information. Our contact information appears at the end of this Privacy Statement.
+                </p>
+                <p>Table of Contents</p>
+                <ul className="list-disc list-inside space-y-3 ml-10">
+                    <li>
+                        <a
+                            href="#Data_Collection"
+                            className="text-muted-foreground hover:text-current focus:text-current"
+                        >
+                            What data do we collect?
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#How_is_Data_Collected"
+                            className="text-muted-foreground hover:text-current focus:text-current"
+                        >
+                            How do we collect your data?
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#Data_Usage" className="text-muted-foreground hover:text-current focus:text-current">
+                            How will we use your data?
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#Data_Storage" className="text-muted-foreground hover:text-current focus:text-current">
+                            How do we store your data?
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#Data_Protection_Rights"
+                            className="text-muted-foreground hover:text-current focus:text-current"
+                        >
+                            What are your data protection rights?
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#Log_Data" className="text-muted-foreground hover:text-current focus:text-current">
+                            What log data do we collect?
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#Advertisements"
+                            className="text-muted-foreground hover:text-current focus:text-current"
+                        >
+                            Advertisements
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#Advertisements2"
+                            className="text-muted-foreground hover:text-current focus:text-current"
+                        >
+                            Advertisements Part 2
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#Cookies" className="text-muted-foreground hover:text-current focus:text-current">
+                            What are cookies?
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#Usage_of_Cookies"
+                            className="text-muted-foreground hover:text-current focus:text-current"
+                        >
+                            How do we use cookies?
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#Types_of_Cookies_Used"
+                            className="text-muted-foreground hover:text-current focus:text-current"
+                        >
+                            What types of cookies do we use?
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#Managing_Cookies"
+                            className="text-muted-foreground hover:text-current focus:text-current"
+                        >
+                            How to manage your cookies
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#External_Websites"
+                            className="text-muted-foreground hover:text-current focus:text-current"
+                        >
+                            Privacy policies of other websites
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#Privacy_Policy_Modifications"
+                            className="text-muted-foreground hover:text-current focus:text-current"
+                        >
+                            Changes to our privacy policy
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#Contact_Info" className="text-muted-foreground hover:text-current focus:text-current">
+                            How to contact us
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <section className="space-y-5 my-5">
+                <h1 id="Data_Collection" className="text-3xl font-bold text-primary">
+                    What data do we collect?
+                </h1>
+                <p>UltrasHub collects the following data:</p>
+                <ul className="list-disc list-inside space-y-3 ml-10">
+                    <li>Email</li>
+                    <li>Username</li>
+                    <li>Information about favorite team(s) and competition(s)</li>
+                    <li>Your current settings preferences</li>
+                </ul>
+                <p>UltrasHub does NOT collect:</p>
+                <ul className="list-disc list-inside space-y-3 ml-10">
+                    <li>Personally Identifiable Information</li>
+                </ul>
+                <p>
+                    <i>
+                        If you believe a certain data type is missing from the lists above, feel free to contact us and
+                        we will answer any questions and update the privacy policy.
+                    </i>
+                </p>
+            </section>
+
+            <section className="space-y-5 my-5">
+                <h1 id="How_is_Data_Collected" className="text-3xl font-bold text-primary">
+                    How do we collect your data?
+                </h1>
+                <p>You directly provide most of the data we collect. We collect data and process data when you:</p>
+                <ul className="list-disc list-inside space-y-3 ml-10">
+                    <li>Create an account</li>
+                    <li>Favorite a new team or competition</li>
+                    <li>Change settings preferences on the website</li>
+                </ul>
+            </section>
+
+            <section className="space-y-5 my-5">
+                <h1 id="Data_Usage" className="text-3xl font-bold text-primary">
+                    How will we use your data?
+                </h1>
+                <p>UltrasHub collects your data so that we can:</p>
+                <ul className="list-disc list-inside space-y-3 ml-10">
+                    <li>Display your favorite team(s) and/or competition(s) at the top of search results</li>
+                    <li>Remember your settings preferences</li>
+                </ul>
+            </section>
+
+            <section className="space-y-5 my-5">
+                <h1 id="Data_Storage" className="text-3xl font-bold text-primary">
+                    How do we store your data?
+                </h1>
+                <p>UltrasHub securely stores your data using PostgreSQL.</p>
+            </section>
+            <section className="space-y-5 my-5">
+                <h1 id="Data_Protection_Rights" className="text-3xl font-bold text-primary">
+                    What are your data protection rights?
+                </h1>
+                <p>
+                    UltrasHub would like to make sure you are fully aware of all of your data protection rights. Every
+                    user is entitled to the following:
+                </p>
+                <ul className="list-disc list-inside space-y-3 ml-10">
+                    <li>
+                        The right to access – You have the right to request UltrasHub for copies of your personal data.
+                        We may limit the number of times this request can be made to depending on the size of the
+                        request.
+                    </li>
+                    <li>
+                        The right to rectification – You have the right to request that UltrasHub correct any
+                        information you believe is inaccurate. You also have the right to request UltrasHub to complete
+                        the information you believe is incomplete.
+                    </li>
+                    <li>
+                        The right to erasure – You have the right to request that UltrasHub erase your personal data,
+                        under certain conditions.
+                    </li>
+                    <li>
+                        The right to restrict processing – You have the right to request that UltrasHub restrict the
+                        processing of your personal data, under certain conditions.
+                    </li>
+                    <li>
+                        The right to object to processing – You have the right to object to UltrasHub processing of your
+                        personal data, under certain conditions.
+                    </li>
+                    <li>
+                        The right to data portability – You have the right to request that UltrasHub transfer the data
+                        that we have collected to another organization, or directly to you, under certain conditions.
+                    </li>
+                </ul>
+            </section>
+
+            <section className="space-y-5 my-5">
+                <h1 id="Log_Data" className="text-3xl font-bold text-primary">
+                    What log data do we collect?
+                </h1>
+                <p>
+                    Like most websites, UltrasHub collects information that your browser sends whenever you visit the
+                    website. This data may include internet protocol (IP) addresses, browser type, Internet Service
+                    Provider (ISP), date and time stamp, referring/exit pages, and time spent on each page.{" "}
+                    <b>THIS DATA DOES NOT CONTAIN ANY PERSONALLY IDENTIFIABLE INFORMATION. </b>We use this information
+                    for analyzing trends, administering the site, tracking users' movement on the website, and gathering
+                    demographic information.
+                </p>
+                <p>In our case, this service is provided by Google and Digital Ocean Analytics.</p>
+            </section>
+
+            <section className="space-y-5 my-5">
+                <h1 id="Advertisements" className="text-3xl font-bold text-primary">
+                    Advertisements
+                </h1>
+                <p>
+                    <i>Currently, UltrasHub does NOT display any advertisements.</i>
+                </p>
+            </section>
+
+            <section className="space-y-5 my-5">
+                <h1 id="Cookies" className="text-3xl font-bold text-primary">
+                    What are cookies?
+                </h1>
+                <p>
+                    Cookies are text files placed on your computer to collect standard Internet log information and
+                    visitor behavior information. When you visit our websites, we may collect information from you
+                    automatically through cookies or similar technology.
+                </p>
+                <p>
+                    For further information, see{" "}
+                    <a
+                        href="https://en.wikipedia.org/wiki/HTTP_cookie"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        className="text-muted-foreground hover:text-current focus:text-current"
+                    >
+                        HTTP cookie
+                    </a>
+                    &nbsp; on Wikipedia.
+                </p>
+            </section>
+
+            <section className="space-y-5 my-5">
+                <h1 id="Usage_of_Cookies" className="text-3xl font-bold text-primary">
+                    How do we use cookies?
+                </h1>
+                <p>UltrasHub uses cookies in a range of ways to improve your experience on our website, including:</p>
+                <ul className="list-disc list-inside space-y-3 ml-10">
+                    <li>Keeping you signed in</li>
+                    <li>Remembering your favorite team(s) and competition(s)</li>
+                    <li>Remembering your settings preferences</li>
+                </ul>
+            </section>
+
+            <section className="space-y-5 my-5">
+                <h1 id="Types_of_Cookies_Used" className="text-3xl font-bold text-primary">
+                    What types of cookies do we use?
+                </h1>
+                <p>
+                    There are a number of different types of cookies; however, our website uses functionality cookies.
+                    UltrasHub uses these cookies so we recognize you on our website and remember your previously
+                    selected settings.
+                </p>
+            </section>
+
+            <section className="space-y-5 my-5">
+                <h1 id="Managing_Cookies" className="text-3xl font-bold text-primary">
+                    How to manage your cookies
+                </h1>
+                <p>
+                    You can set your browser not to accept cookies, and the above website tells you how to remove
+                    cookies from your browser. However, in a few cases, some of our website features may behave
+                    unexpectedly or fail to function as a result.
+                </p>
+            </section>
+
+            <section className="space-y-5 my-5">
+                <h1 id="External_Websites" className="text-3xl font-bold text-primary">
+                    Privacy policies of other websites
+                </h1>
+                <p>
+                    UltrasHub contains links to other external websites.{" "}
+                    <b>
+                        <u>
+                            Our privacy policy only applies to our website, so if you click on a link to another
+                            website, you should read their privacy policy.
+                        </u>
+                    </b>
+                </p>
+            </section>
+
+            <section className="space-y-5 my-5">
+                <h1 id="Privacy_Policy_Modifications" className="text-3xl font-bold text-primary">
+                    Changes to our privacy policy
+                </h1>
+                <p>
+                    UltrasHub keeps its privacy policy under regular review and places any updates on this web page. The
+                    UltrasHub privacy policy may be subject to change at any given time without notice.
+                </p>
+            </section>
+
+            <section className="space-y-5 my-5">
+                <h1 id="Contact_Info" className="text-3xl font-bold text-primary">
+                    How to contact us
+                </h1>
+                <p>
+                    If you have any questions about UltrasHub’s privacy policy, the data we hold on you, or you would
+                    like to exercise one of your data protection rights, please do not hesitate to contact us.
+                </p>
+                <p>
+                    General inquiries:{" "}
+                    <a href="mailto: contact@ultrashub.io" target="_blank" rel="noopener noreferrer">
+                        contact@ultrashub.io
+                    </a>
+                </p>
+            </section>
         </>
     );
 };
