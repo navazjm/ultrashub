7a1cd40b876ae8e5b5d2e1ac03c3c7465bedecdf
navazjm <michaelnavs@gmail.com>
2024-02-15 21:50:54 -0600

    web - be able to search all competitions based on name

--- web/src/components/features/competitions/competitions.hooks.tsx
+++ web/src/components/features/competitions/competitions.hooks.tsx
@@ -1,10 +1,19 @@
-import { ICompetition, ICompetitionResponse } from "@/components/common/api-football-response";
-import axios from "@/lib/axios";
-import { AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
+import { AxiosResponse } from "axios";
+import { ICompetition, ICompetitionResponse } from "@/components/common/api-football-response";
+import { TOP_COMPS_IDS } from "@/components/common/types";
+import axios from "@/lib/axios";
+
+interface ICompetitionsData {
+    allCompetitions: ICompetition[];
+    setAllCompetitions: React.Dispatch<React.SetStateAction<ICompetition[]>>;
+    filteredCompetitions: ICompetition[];
+    setFilteredCompetitions: React.Dispatch<React.SetStateAction<ICompetition[]>>;
+}
 
 export const useCompetitions = () => {
-    const [competitions, setCompetitions] = useState<ICompetition[] | null>(null);
+    const [allCompetitions, setAllCompetitions] = useState<ICompetition[]>([]);
+    const [filteredCompetitions, setFilteredCompetitions] = useState<ICompetition[]>([]);
     const [status, setStatus] = useState<"loading" | "success" | "error">("loading");
 
     useEffect(() => {
@@ -20,7 +29,30 @@ export const useCompetitions = () => {
                 }
                 const newCompetitions = resp.data.response;
 
-                setCompetitions(newCompetitions);
+                const topCompetitions: ICompetition[] = [];
+                const nonTopCompetitions: ICompetition[] = [];
+
+                // group competitons by if it is a top com
+                newCompetitions.forEach((comp) => {
+                    TOP_COMPS_IDS.includes(comp.league.id) ? topCompetitions.push(comp) : nonTopCompetitions.push(comp);
+                });
+
+                // within each sub group of top and non-top comps, group by country name
+                topCompetitions.sort((a, b) => {
+                    const aCompName = `${a.country.name} ${a.league.name}`;
+                    const bCompName = `${b.country.name} ${b.league.name}`;
+                    return aCompName.localeCompare(bCompName);
+                });
+                nonTopCompetitions.sort((a, b) => {
+                    const aCompName = `${a.country.name} ${a.league.name}`;
+                    const bCompName = `${b.country.name} ${b.league.name}`;
+                    return aCompName.localeCompare(bCompName);
+                });
+
+                const competitionsDisplayList = [...topCompetitions, ...nonTopCompetitions];
+
+                setAllCompetitions(competitionsDisplayList);
+                setFilteredCompetitions(competitionsDisplayList);
                 setStatus("success");
             } catch (err) {
                 setStatus("error");
@@ -32,6 +64,15 @@ export const useCompetitions = () => {
 
     const isLoading = status === "loading";
     const isError = status === "error";
+    const data: ICompetitionsData | null =
+        isLoading || isError
+            ? null
+            : {
+                  allCompetitions,
+                  setAllCompetitions,
+                  filteredCompetitions,
+                  setFilteredCompetitions,
+              };
 
-    return [competitions, isLoading, isError] as const;
+    return [data, isLoading, isError] as const;
 };
--- web/src/components/features/competitions/competitions.tsx
+++ web/src/components/features/competitions/competitions.tsx
@@ -1,18 +1,17 @@
 import { Spinner } from "@/components/ui/spinner";
 import { ErrorComponent } from "@/components/common/error/error";
 import { useCompetitions } from "./competitions.hooks";
-import { ICompetition } from "@/components/common/api-football-response";
-import { TOP_COMPS_IDS } from "../matches/matches.types";
 import { CompetitionsItemComponent } from "./competitions-item/competitions-item";
+import { CompetitionsSearchComponent } from "./competitions-search/competitions-search";
 
 export const CompetitionsComponent = () => {
-    const [competitions, isLoading, isError] = useCompetitions();
+    const [data, isLoading, isError] = useCompetitions();
 
     if (isLoading) {
         return <Spinner />;
     }
 
-    if (isError || !competitions) {
+    if (isError || !data) {
         return (
             <ErrorComponent
                 backNavTitle="Error!"
@@ -20,37 +19,23 @@ export const CompetitionsComponent = () => {
             />
         );
     }
-
-    const topCompetitions: ICompetition[] = [];
-    const nonTopCompetitions: ICompetition[] = [];
-
-    // group competitons by if it is a top com
-    competitions.forEach((comp) => {
-        TOP_COMPS_IDS.includes(comp.league.id) ? topCompetitions.push(comp) : nonTopCompetitions.push(comp);
-    });
-
-    // within each sub group of top and non-top comps, group by country name
-    topCompetitions.sort((a, b) => {
-        const aCompName = `${a.country.name} ${a.league.name}`;
-        const bCompName = `${b.country.name} ${b.league.name}`;
-        return aCompName.localeCompare(bCompName);
-    });
-    nonTopCompetitions.sort((a, b) => {
-        const aCompName = `${a.country.name} ${a.league.name}`;
-        const bCompName = `${b.country.name} ${b.league.name}`;
-        return aCompName.localeCompare(bCompName);
-    });
-
-    const competitionsDisplayList = [...topCompetitions, ...nonTopCompetitions];
-
     return (
-        <section className="overflow-hidden">
-            <h3 className="text-2xl font-bold mb-5">All Competitions</h3>
+        <section className="space-y-5">
+            <h3 className="text-2xl font-bold">All Competitions</h3>
+
+            <CompetitionsSearchComponent
+                allCompetitions={data.allCompetitions}
+                setFilteredCompetitions={data.setFilteredCompetitions}
+            />
 
             <section className="grid grid-cols-1 lg:grid-cols-3 gap-1">
-                {competitionsDisplayList.map((competition) => (
-                    <CompetitionsItemComponent competition={competition} key={competition.league.id} />
-                ))}
+                {data.filteredCompetitions.length > 0 ? (
+                    data.filteredCompetitions.map((competition) => (
+                        <CompetitionsItemComponent competition={competition} key={competition.league.id} />
+                    ))
+                ) : (
+                    <ErrorComponent errorMessage="Wow, such empty" />
+                )}
             </section>
         </section>
     );
