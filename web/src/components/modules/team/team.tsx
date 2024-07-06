6c8899c5831dfe3dfa302af314040c0153a77d7c
navazjm <michaelnavs@gmail.com>
2024-07-06 16:06:34 -0500

    web - team page - add button to add team to user favorites

--- web/src/components/modules/team/team.tsx
+++ web/src/components/modules/team/team.tsx
@@ -5,6 +5,14 @@ import { ApiFootballLogoComponent } from "@/components/shared/api-football-logo/
 import { useTeam } from "./team.hooks";
 import { TeamMatchesComponent } from "./team-matches/team-matches";
 import { TeamSquadComponent } from "./team-squad/team-squad";
+import { useAuthContext, useAxiosPrivate } from "@/common/auth/auth.hooks";
+import { Heart } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/shadcn";
+import { useToast } from "@/components/ui/use-toast";
+import { IUsersPreferencesResponse } from "@/common/auth/auth.types";
+import { AxiosResponse } from "axios";
+import { useState } from "react";
 
 interface ITeamComponentProps {
     teamID: string;
@@ -12,6 +20,10 @@ interface ITeamComponentProps {
 
 export const TeamComponent = (props: ITeamComponentProps) => {
     const [data, isLoading, isError] = useTeam(props.teamID);
+    const authCtx = useAuthContext();
+    const axiosPrivate = useAxiosPrivate();
+    const { toast } = useToast();
+    const [isUpdatingUserPreferences, setIsUpdatingUserPreferences] = useState<boolean>(false);
 
     if (isLoading) {
         return <Spinner />;
@@ -21,20 +33,79 @@ export const TeamComponent = (props: ITeamComponentProps) => {
         return <ErrorComponent title="Error!" message="No team data was found. Refresh the page or try again later." />;
     }
 
+    const updateUserFavoriteTeams = async (teamID: number) => {
+        const userFavoriteTeams = authCtx.usersPreferences.favoriteTeams;
+        const userFavoriteTeamIdx = userFavoriteTeams.findIndex((id) => id === teamID);
+        if (userFavoriteTeamIdx == -1) {
+            if (authCtx.usersPreferences.favoriteTeams.length >= 5) {
+                toast({
+                    variant: "destructive",
+                    title: "Error!",
+                    description: "Cannot add more than 5 teams to favorites.",
+                });
+                return;
+            }
+
+            userFavoriteTeams.push(teamID);
+        } else {
+            userFavoriteTeams.splice(userFavoriteTeamIdx, 1);
+        }
+        setIsUpdatingUserPreferences(true);
+
+        try {
+            const resp: AxiosResponse<IUsersPreferencesResponse> = await axiosPrivate.patch("/users/preferences", {
+                favoriteTeams: userFavoriteTeams,
+            });
+            console.log(resp.data.data);
+            authCtx.setUsersPreferences(resp.data.data);
+        } catch (err) {
+            toast({ variant: "destructive", title: "Error!", description: "Failed to update user's favorite teams." });
+        } finally {
+            setIsUpdatingUserPreferences(false);
+        }
+    };
+
     return (
         <section className="space-y-5">
-            <section className="flex items-center gap-2">
-                {data.team && data.team.team.logo && (
-                    <ApiFootballLogoComponent
-                        src={data.team.team.logo}
-                        alt={`${data.team.team.name} logo`}
-                        width={100}
-                        height={100}
-                    />
-                )}
-                <section className="flex flex-col gap-1">
-                    <h1 className="font-black text-3xl">{data.team.team.name}</h1>
-                    <h3 className="font-thin">est. {data.team.team.founded}</h3>
+            <section className="flex items-center justify-between">
+                <section className="flex items-center gap-2">
+                    {data.team.team.logo && (
+                        <ApiFootballLogoComponent
+                            src={data.team.team.logo}
+                            alt={`${data.team.team.name} logo`}
+                            width={100}
+                            height={100}
+                        />
+                    )}
+                    <section className="flex flex-col gap-1">
+                        <h1 className="font-black text-3xl">{data.team.team.name}</h1>
+                        <h3 className="font-thin">est. {data.team.team.founded}</h3>
+                    </section>
+                </section>
+                <section>
+                    {authCtx.firebaseUser && (
+                        <>
+                            <Button
+                                variant="ghost"
+                                size="icon"
+                                onClick={() => data.team && updateUserFavoriteTeams(data.team.team.id)}
+                                disabled={isUpdatingUserPreferences}
+                            >
+                                {isUpdatingUserPreferences ? (
+                                    <Spinner />
+                                ) : (
+                                    <Heart
+                                        className={cn(
+                                            "h-[2rem] w-[2rem] transition-all stroke-primary",
+                                            authCtx.usersPreferences.favoriteTeams.includes(data.team.team.id) &&
+                                            "fill-primary",
+                                        )}
+                                    />
+                                )}
+                                <span className="sr-only">Add favorite team</span>
+                            </Button>
+                        </>
+                    )}
                 </section>
             </section>
 
